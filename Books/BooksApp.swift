dc00ddfddf718fc7d638c482f709f19d7eb47a96
Serg Basin <shoom.pozitiv@gmail.com>
2024-02-26 15:38:44 +0200

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Sergey Basin on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
